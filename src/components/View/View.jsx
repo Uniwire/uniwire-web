e71c77d7943e0eab0fe1a2031a2f3d1261c3044d
gabihtoledo0 <gabriela.prado@youse.com.br>
2020-09-17 16:14:39 -0300

    logo adjustment

--- src/components/View/View.jsx
+++ src/components/View/View.jsx
@@ -1,21 +1,24 @@
 import styled from "styled-components";
 import PropType from "prop-types";
-import designTokens from "../../config/designTokens"
+import designTokens from "../../config/designTokens";
 
 const View = styled.div`
   display: flex;
   justify-content: ${(props) => (props.justify ? props.justify : "center")};
   align-items: center;
-  flex-direction: ${(props) => props.row || props.column ? "row" || "column" : "row"};
+  flex-direction: ${(props) =>
+    props.row || props.column ? "row" || "column" : "row"};
   position: relative;
   left: ${(props) => (props.left ? props.left : "0")}px;
+  margin-bottom: ${(props) =>
+    props.marginBottom ? props.marginBottom : "0"}px;
 
   @media screen and (min-width: ${designTokens.breakpoints.tablet}px) {
-    left: ${(props) => (props.leftTablet ? props.leftTablet : "0")}px;
+    left: ${(props) => (props.tabletLeft ? props.tabletLeft : "0")}px;
   }
 
   @media screen and (min-width: ${designTokens.breakpoints.desktop}px) {
-    left: ${(props) => (props.leftDesktop ? props.leftDesktop : "0")}px;
+    left: ${(props) => (props.desktopLeft ? props.desktopLeft : "0")}px;
   }
 `;
 
@@ -24,6 +27,9 @@ View.propTypes = {
   left: PropType.number,
   row: PropType.boolean,
   column: PropType.boolean,
+  marginBottom: PropType.number,
+  tabletLeft: PropType.number,
+  desktopLeft: PropType.number,
 };
 
 export default View;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -7,13 +7,15 @@ import { ButtonPrimary } from "../components/Button/Button";
 import Title from "../components/Title/Title";
 import View from "../components/View/View";
 import { Small } from "../components/Text/Text";
+import { StyledColumn, ColumnContainer } from "../components/Grid/Grid";
 import { useForm } from "react-hook-form";
 
 const Container = styled.div`
-  background-color: ${designTokens.colors.grey000};
+  background: ${designTokens.colors.grey000};
   width: 100%;
   height: auto;
   padding: 15px 15px 30px 15px;
+  box-shadow: ${designTokens.shadows.dark.idle};
 
   @media screen and (min-width: ${designTokens.breakpoints.desktop}px) {
     width: 500px;
@@ -49,79 +51,86 @@ function Login() {
 
   return (
     <>
-      <View>
+      <View justify="left" desktopLeft="30" marginBottom="60">
         <Title
           color={designTokens.colors.black100}
-          weight="medium"
+          weight="regular"
           size="default"
         >
           uniwire
         </Title>
-        <img src={LogoImage} width={"100"} alt="logo uniwire" />
+        <img src={LogoImage} width={"70"} alt="logo uniwire" />
       </View>
-      <View column>
-        <Container>
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <View>
-              <Title size="small" color={designTokens.colors.grey900}>
-                Já é cadastrado?
-              </Title>
-            </View>
-            <View>
-              <Input
-                withoutMarginTop
-                autocomplete="off"
-                widthDesktop={400}
-                type="email"
-                name="email"
-                id="inputEmail"
-                placeholder="Email"
-                ref={register({
-                  required: "Preencha o campo email",
-                  pattern: {
-                    value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
-                    message: "Preencha com um email válido",
-                  },
-                })}
-              />
-            </View>
-            <View justify="left" left={5} leftTablet={130} leftDesktop={25}>
-              {errors.email && (
-                <Small className="error">{errors.email.message}</Small>
-              )}
-            </View>
-            <View>
-              <Input
-                widthDesktop={400}
-                type="password"
-                name="password"
-                id="inputSenha"
-                placeholder="Senha"
-                ref={register({
-                  required: "Preencha o campo senha",
-                  // pattern: {
-                  //   value: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
-                  //   message: "Preencha com uma senha válida",
-                  // },
-                })}
-              />
-            </View>
-            <View justify="left" left={5} leftTablet={130} leftDesktop={25}>
-              {errors.password && (
-                <Small className="error">{errors.password.message}</Small>
-              )}
-            </View>
-            <View justify="left" left={5}>
-              {session.message && (
-                <Small className="error">{session.message}</Small>
-              )}
-            </View>
-            <View>
-              <ButtonPrimary withoutMarginTop>Entrar</ButtonPrimary>
-            </View>
-          </form>
-        </Container>
-      </View>
+      <StyledColumn>
+        <ColumnContainer desktopSize={6} tabletSize={6} size={6}>
+          
+        </ColumnContainer>
+        <ColumnContainer desktopSize={6} tabletSize={12} size={12}>
+          <View>
+            <Container>
+              <form onSubmit={handleSubmit(onSubmit)}>
+                <View>
+                  <Title size="small" color={designTokens.colors.grey900}>
+                    Já é cadastrado?
+                  </Title>
+                </View>
+                <View>
+                  <Input
+                    withoutMarginTop
+                    autocomplete="off"
+                    widthDesktop={400}
+                    type="email"
+                    name="email"
+                    id="inputEmail"
+                    placeholder="Email"
+                    ref={register({
+                      required: "Preencha o campo email",
+                      pattern: {
+                        value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                        message: "Preencha com um email válido",
+                      },
+                    })}
+                  />
+                </View>
+                <View justify="left" left={5} leftTablet={130} leftDesktop={25}>
+                  {errors.email && (
+                    <Small className="error">{errors.email.message}</Small>
+                  )}
+                </View>
+                <View>
+                  <Input
+                    widthDesktop={400}
+                    type="password"
+                    name="password"
+                    id="inputSenha"
+                    placeholder="Senha"
+                    ref={register({
+                      required: "Preencha o campo senha",
+                      // pattern: {
+                      //   value: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+                      //   message: "Preencha com uma senha válida",
+                      // },
+                    })}
+                  />
+                </View>
+                <View justify="left" left={5} leftTablet={130} leftDesktop={25}>
+                  {errors.password && (
+                    <Small className="error">{errors.password.message}</Small>
+                  )}
+                </View>
+                <View justify="left" left={5}>
+                  {session.message && (
+                    <Small className="error">{session.message}</Small>
+                  )}
+                </View>
+                <View>
+                  <ButtonPrimary withoutMarginTop>Entrar</ButtonPrimary>
+                </View>
+              </form>
+            </Container>
+          </View>
+        </ColumnContainer>
+      </StyledColumn>
     </>
   );
 }
