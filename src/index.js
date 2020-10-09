615d0acc22aa9103462333c89ae87c8ee64d2397
João Paulo Motta <joao.paulo.fm@hotmail.com>
2020-10-09 10:33:37 -0300

    Add material-ui

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import { render } from "react-dom";
 import Routes from "./Routes";
 import GlobalStyles from "./components/GlobalStyles/GlobalStyles";
+import 'fontsource-roboto';
 
 function App() {
   return (
