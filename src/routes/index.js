def3cb7f8e58a91f6932f860263c188ee6086abe
gabihtoledo0 <gabriela.prado@youse.com.br>
2020-10-16 17:22:02 -0300

    adjusment deploy

---
+++ src/routes/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import Login from "../pages/Login";
+import HomePage from "../pages/HomePage";
+
+export const Routes = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={HomePage} />
+        <Route path="/login" exact component={Login} />
+        <Route path="/homepage" exact component={HomePage} />
+      </Switch>
+    </BrowserRouter>
+  );
+}
