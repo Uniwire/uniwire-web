4f0bc4865650e8fd738de90b6e7970577b4b4904
gabihtoledo0 <gabriela.prado@youse.com.br>
2020-10-16 09:58:04 -0300

    conflict adjustment

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import { render } from "react-dom";
 import Routes from "./Routes";
 import GlobalStyles from "./components/GlobalStyles/GlobalStyles";
-import 'fontsource-roboto';
 
 function App() {
   return (
