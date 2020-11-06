56661add92afe8865b0caa482cee0a2cc84f4d4a
gabihtoledo0 <gabriela.prado@youse.com.br>
2020-11-06 10:45:41 -0300

    added map on the main screen

--- src/pages/UniversityMap.jsx
+++ src/pages/UniversityMap.jsx
@@ -1,7 +1,58 @@
+/* eslint-disable react/style-prop-object */
 import React from "react";
+// import Leaflet from 'leaflet';
+import "leaflet/dist/leaflet.css";
+import LogoImage from "../static/images/logo-black.svg";
+import { Map, TileLayer } from "react-leaflet";
+import designTokens from "../config/designTokens";
 
 function UniversityMap() {
-  return <div>Mapa</div>
+  return (
+    <>
+      <div
+        style={{
+          width: "100vw",
+          height: "100vh",
+          position: "relative",
+          display: "flex",
+        }}
+      >
+        <aside
+          style={{
+            width: "320px",
+            background: `${designTokens.colors.grey800}`,
+            padding: "80px",
+            opacity: 8,
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-between",
+          }}
+        >
+          <header>
+            <img src={LogoImage} width="60px" alt="logo bike" />
+            <h2 className="text-gray-700">Selecione a estação que desejar</h2>
+            <p className="text-gray-700">
+              Procure as estações que você queira retirar ou depositar sua bike
+              :)
+            </p>
+          </header>
+          <footer>
+            <strong>Springfield</strong>
+            <span>Oregon</span>
+          </footer>
+        </aside>
+        <Map
+          center={[-23.644495, -46.528175]}
+          zoom={17}
+          style={{ width: "100%", height: "100%" }}
+        >
+          <TileLayer
+            url={`https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
+          />
+        </Map>
+      </div>
+    </>
+  );
 }
 
 export default UniversityMap;
